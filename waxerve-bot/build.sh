cf60f3936b2665f9745c7743d6a2458786c95804
Boterman B (Bas) <bas.boterman@intraffic.nl>
2025-12-23 13:06:54 +0100

    Removed old source code.

--- waxerve-bot/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script builds the Waxerve bot Docker image.
-VERSION=$(mvn help:evaluate -Dexpression="project.version" -q -DforceStdout 2> $null)
-docker build --build-arg VERSION=$VERSION -t waxerve-bot:$VERSION .

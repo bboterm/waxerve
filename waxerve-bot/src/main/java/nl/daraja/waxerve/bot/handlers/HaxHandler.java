7ecbeaa5dcf9b714aaf4f49a8834aeec54d84e0c
bboterm <2551130+bboterm@users.noreply.github.com>
2025-12-24 19:47:57 +0100

    Textual fixes. Improved HaxHandler.

--- waxerve-bot/src/main/java/nl/daraja/waxerve/bot/handlers/HaxHandler.java
+++ waxerve-bot/src/main/java/nl/daraja/waxerve/bot/handlers/HaxHandler.java
@@ -7,14 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
 import nl.daraja.waxerve.util.I18n;
 
 public class HaxHandler implements InstructionHandler {
-
-    public static final String HANDLER_TIME_1337 = "handler.time.1337";
-    public static final String HANDLER_TIME_2337 = "handler.time.2337";
-    public static final String HANDLER_TIME_0337 = "handler.time.0337";
-    public static final String HANDLER_TIME_0314 = "handler.time.0314";
 
     @Override
     public List<String> handle(String instruction) {
@@ -25,23 +21,29 @@ public class HaxHandler implements InstructionHandler {
         // Format it correctly
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-        Map<LocalTime, String> alertMap = Map.ofEntries(Map.entry(LocalTime.of(13, 37), I18n.get(HANDLER_TIME_1337)),
-                Map.entry(LocalTime.of(13, 38), I18n.get(HANDLER_TIME_1337)),
-                Map.entry(LocalTime.of(13, 39), I18n.get(HANDLER_TIME_1337)),
-                Map.entry(LocalTime.of(23, 37), I18n.get(HANDLER_TIME_2337)),
-                Map.entry(LocalTime.of(23, 38), I18n.get(HANDLER_TIME_2337)),
-                Map.entry(LocalTime.of(23, 39), I18n.get(HANDLER_TIME_2337)),
-                Map.entry(LocalTime.of(3, 37), I18n.get(HANDLER_TIME_0337)),
-                Map.entry(LocalTime.of(3, 38), I18n.get(HANDLER_TIME_0337)),
-                Map.entry(LocalTime.of(3, 39), I18n.get(HANDLER_TIME_0337)),
-                Map.entry(LocalTime.of(3, 14), I18n.get(HANDLER_TIME_0314)),
-                Map.entry(LocalTime.of(3, 15), I18n.get(HANDLER_TIME_0314)),
-                Map.entry(LocalTime.of(3, 16), I18n.get(HANDLER_TIME_0314)));
+        Map<LocalTime, Integer> correctionMap = Map.ofEntries(Map.entry(LocalTime.of(13, 37), 0),
+                Map.entry(LocalTime.of(13, 38), -1),
+                Map.entry(LocalTime.of(13, 39), -2),
+                Map.entry(LocalTime.of(23, 37), 0),
+                Map.entry(LocalTime.of(23, 38), -1),
+                Map.entry(LocalTime.of(23, 39), -2),
+                Map.entry(LocalTime.of(3, 37), 0),
+                Map.entry(LocalTime.of(3, 38), -1),
+                Map.entry(LocalTime.of(3, 39), -2),
+                Map.entry(LocalTime.of(3, 14), 0),
+                Map.entry(LocalTime.of(3, 15), -1),
+                Map.entry(LocalTime.of(3, 16), -2));
 
-        Optional.ofNullable(alertMap.get(now.truncatedTo(ChronoUnit.MINUTES)))
-                .ifPresentOrElse(s -> {
-                            result.add(I18n.get("handler.time.current", now.format(formatter)));
-                            result.add(I18n.get("handler.time.omg", s));
+        Map<LocalTime, String> alertMap = Map.of(LocalTime.of(13, 37), I18n.get("handler.time.1337"),
+                LocalTime.of(23, 37), I18n.get("handler.time.2337"),
+                LocalTime.of(3, 37), I18n.get("handler.time.0337"),
+                LocalTime.of(3, 14), I18n.get("handler.time.0314"));
+
+        Optional.ofNullable(correctionMap.get(now.truncatedTo(ChronoUnit.MINUTES)))
+                .ifPresentOrElse(i -> {
+                            LocalTime correction = now.plusMinutes(i);
+                            result.add(I18n.get("handler.time.current", correction.format(formatter)));
+                            result.add(I18n.get("handler.time.omg", alertMap.get(correction.truncatedTo(ChronoUnit.MINUTES))));
                         },
                         () -> result.add(I18n.get("handler.hax.haxen"))
                 );

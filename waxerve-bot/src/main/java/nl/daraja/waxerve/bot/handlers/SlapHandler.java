343ead5debcce971c95b33d4d95332d39fb4a383
bboterm <2551130+bboterm@users.noreply.github.com>
2025-12-24 10:59:06 +0100

    Implemented SlapHandler.

--- waxerve-bot/src/main/java/nl/daraja/waxerve/bot/handlers/SlapHandler.java
+++ waxerve-bot/src/main/java/nl/daraja/waxerve/bot/handlers/SlapHandler.java
@@ -9,7 +9,9 @@ public class SlapHandler implements InstructionHandler {
     public List<String> handle(String instruction) {
         List<String> result = new ArrayList<>();
 
-        result.add("Slaps around a bit with a large trout.");
+        String slappee = instruction.length() > 6 ? instruction.substring(6) : "";
+
+        result.add("Slaps " + slappee + " around a bit with a large trout.");
 
         return result;
     }

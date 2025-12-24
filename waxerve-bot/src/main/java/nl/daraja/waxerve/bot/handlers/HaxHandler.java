7281b3c49bc585739494cde483b0275196ccad35
bboterm <2551130+bboterm@users.noreply.github.com>
2025-12-24 10:47:59 +0100

    Implemented TimeHandler and HaxHandler.

--- waxerve-bot/src/main/java/nl/daraja/waxerve/bot/handlers/HaxHandler.java
+++ waxerve-bot/src/main/java/nl/daraja/waxerve/bot/handlers/HaxHandler.java
@@ -1,7 +1,12 @@
 package nl.daraja.waxerve.bot.handlers;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class HaxHandler implements InstructionHandler {
 
@@ -9,8 +14,33 @@ public class HaxHandler implements InstructionHandler {
     public List<String> handle(String instruction) {
         List<String> result = new ArrayList<>();
 
-        result.add("Mama, ze haxen!!1 :(");
+        // Get system time
+        LocalTime now = LocalTime.now();
+        // Format it correctly
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        // Add to result
+        result.add("The current time is " + now.format(formatter));
 
+        Map<LocalTime, String> alertMap = Map.ofEntries(Map.entry(LocalTime.of(13, 37), "OMG! It's 1337!"),
+                Map.entry(LocalTime.of(13, 38), "OMG! It's 1337!"),
+                Map.entry(LocalTime.of(13, 39), "OMG! It's 1337!"),
+                Map.entry(LocalTime.of(23, 37), "OMG! It's 2337!"),
+                Map.entry(LocalTime.of(23, 38), "OMG! It's 2337!"),
+                Map.entry(LocalTime.of(23, 39), "OMG! It's 2337!"),
+                Map.entry(LocalTime.of(3, 37), "OMG! It's 0337!"),
+                Map.entry(LocalTime.of(3, 38), "OMG! It's 0337!"),
+                Map.entry(LocalTime.of(3, 39), "OMG! It's 0337!"),
+                Map.entry(LocalTime.of(3, 14), "OMG! It's π o'clock!"),
+                Map.entry(LocalTime.of(3, 15), "OMG! It's π o'clock!"),
+                Map.entry(LocalTime.of(3, 16), "OMG! It's π o'clock!"));
+
+        Optional.ofNullable(alertMap.get(now.truncatedTo(ChronoUnit.MINUTES)))
+                .ifPresentOrElse(s -> {
+                            result.add("The current time is " + now.format(formatter));
+                            result.add(s);
+                        },
+                        () -> result.add("Mama, ze haxen!!1 :(")
+                );
         return result;
     }
 }
--- waxerve-bot/src/main/java/nl/daraja/waxerve/bot/handlers/TimeHandler.java
+++ waxerve-bot/src/main/java/nl/daraja/waxerve/bot/handlers/TimeHandler.java
@@ -2,8 +2,11 @@ package nl.daraja.waxerve.bot.handlers;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class TimeHandler implements InstructionHandler {
 
@@ -18,17 +21,12 @@ public class TimeHandler implements InstructionHandler {
         // Add to result
         result.add("The current time is " + now.format(formatter));
 
-        // Do 1337-checks
-        if (now.equals(LocalTime.of(13, 37))) {
-            result.add("OMG! It's 1337!");
-        } else if (now.equals(LocalTime.of(23, 37))) {
-            result.add("OMG! It's 2337!");
-        } else if (now.equals(LocalTime.of(3, 37))) {
-            result.add("OMG! It's 0337!");
-        } else if (now.equals(LocalTime.of(3, 14))) {
-            result.add("OMG! It's π o'clock!");
-        }
+        Map<LocalTime, String> alertMap = Map.of(LocalTime.of(13, 37), "OMG! It's 1337!",
+                LocalTime.of(23, 37), "OMG! It's 2337!",
+                LocalTime.of(3, 37), "OMG! It's 0337!",
+                LocalTime.of(3, 14), "OMG! It's π o'clock!");
 
+        Optional.ofNullable(alertMap.get(now.truncatedTo(ChronoUnit.MINUTES))).ifPresent(result::add);
         return result;
     }
 }
